47d11291183700918d0b6d364843c63de8181adc
Dennis Tran <dtran221@gmail.com>
2020-06-25 17:29:54 -0400

    Created basic server that functions

---
+++ Server/Models/Events.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class Events
+    {
+        public int? Id {get; set; }
+        public string EventName {get; set;}
+        public DateTime DateOrDay {get; set;}
+        public int StartTime {get; set;}
+        public int EndTime {get; set;}
+        public string TimeZone {get; set;}
+        public string commentToAttendees {get; set;}
+    }
+}
