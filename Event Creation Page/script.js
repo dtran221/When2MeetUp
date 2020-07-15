7468b7ad44b233fe5081efde056085706a48dfaa
Dennis Tran <dtran221@gmail.com>
2020-07-15 17:25:33 -0400

    more refactoring to use moment.js

--- Event Creation Page/script.js
+++ Event Creation Page/script.js
@@ -1,14 +1,19 @@
-var dateToday = moment();
-var displayMonth = dateToday.date(1);
-var selectedDates = new Array();
+const dateToday = moment();
+let displayMonth = dateToday.clone();
+displayMonth.date(1);
+let selectedDates = new Array();
 
+
+/**
+ * Setups the webpage to show the current month and the users timezone
+ */
 function initialSetup() {
-  calendar(displayMonth);
+  calendar(dateToday);
 
-  var temp = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  var mySelect = document.getElementById("TimeZone");
+  let temp = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  let mySelect = document.getElementById("TimeZone");
 
-  for (var i, j = 0; (i = TimeZone.options[j]); j++) {
+  for (let i, j = 0; (i = TimeZone.options[j]); j++) {
     if (i.value == temp) {
       TimeZone.selectedIndex = j;
       break;
@@ -16,44 +21,67 @@ function initialSetup() {
   }
 }
 
+/**
+ * 
+ * @param {*} btn 
+ * @param {*} time 
+ */
 function changeTime(btn, time) {
   document.getElementById(btn).selectedIndex = time;
   timeCheck();
 }
 
+/**
+ * 
+ */
 function displayDaysOfWeek() {
   document.getElementById("calendar").style.display = "none";
   document.getElementById("daysOfWeekDisplay").style.display = "inline-block";
   selectedDates = new Array();
 }
 
+/**
+ * 
+ */
 function displayCalendar() {
   calendar(displayMonth);
-  document.getElementById("selectedDatesToDisplay").innerHTML = "";
+  document.getElementById("selectedDatesToDisplay").innerText = "";
   document.getElementById("calendar").style.display = "inline-block";
   document.getElementById("daysOfWeekDisplay").style.display = "none";
-  var checkboxes = document.getElementsByName("dayOfTheWeek");
+  let checkboxes = document.getElementsByName("dayOfTheWeek");
   checkboxes = Array.prototype.slice.call(checkboxes);
-  for (var i = 0; i < checkboxes.length; i++) {
+  for (let i = 0; i < checkboxes.length; i++) {
     checkboxes[i].classList.remove("active");
   }
 }
 
+/**
+ * 
+ */
 function previousMonth() {
   
   calendar(displayMonth.subtract(1,'month'));
 }
+
+/**
+ * 
+ */
 function nextMonth() {
   
   calendar(displayMonth.add(1,'month'));
 }
 
+/**
+ * 
+ * @param {*} labelName 
+ * @param {*} buttonClicked 
+ */
 function quickSelectButton(labelName, buttonClicked) {
-  var checkboxes = document.getElementsByName(labelName);
+  let checkboxes = document.getElementsByName(labelName);
   checkboxes = Array.prototype.slice.call(checkboxes);
-  var start;
-  var end;
-  var increment;
+  let start;
+  let end;
+  let increment;
   if (buttonClicked == "weekday") {
     checkboxes.pop();
     checkboxes.shift();
@@ -61,35 +89,39 @@ function quickSelectButton(labelName, buttonClicked) {
     checkboxes.splice(1, 5);
   }
 
-  for (var i = 0; i < checkboxes.length; i++)
+  for (let i = 0; i < checkboxes.length; i++)
     if (checkboxes[i].classList.contains("active")) {
       checkboxes[i].classList.remove("active");
     } else {
       checkboxes[i].classList.add("active");
     }
 }
-function calendar(inputMonth) {
-  var d = inputMonth;
-  var weekLoop;
-  var dayLoop;
-  var dayCount = 1;
-  var firstDay = d.startOf('month');
-  var dayElementValue = firstDay;
-  var lastDay = d.endOf('month');
-  var monthAndYear = document.getElementById("calendarTitle");
-  monthAndYear.innerHTML = moment(firstDay).format("MMM YYYY")
+
+/**
+ * 
+ * @param {*} inputDate 
+ */
+function calendar(inputDate) {
+  let weekLoop;
+  let dayLoop;
+  let dayCount = 1;
+  const firstDay = moment(inputDate).startOf('month');
+  let dayElementValue = moment(firstDay);
+  const lastDay = moment(inputDate).endOf('month');
+  let monthAndYear = document.getElementById("calendarTitle");
+  monthAndYear.innerText = firstDay.format("MMM YYYY")
   for (loop = 0; loop < 6; loop++) {
-    var rowDivElement = "row" + loop.toString();
-    var dayLoopStart;
-    var firstRowElement = document.getElementById(rowDivElement);
+    let rowDivElement = "row" + loop.toString();
+    let dayLoopStart;
+    let firstRowElement = document.getElementById(rowDivElement);
     firstRowElement.value = loop;
-    firstRowElement.innerHTML = "";
+    firstRowElement.innerText = "";
     firstRowElement.addEventListener("click", selectCalendarRow);
     dayElement = firstRowElement.nextElementSibling;
     if (loop == 0) {
       dayLoopStart = moment(firstDay).day();
       for (dayLoop = 0; dayLoop < dayLoopStart; dayLoop++) {
-        dayElement.innerHTML = "";
+        dayElement.innerText = "";
         if (dayElement.classList.contains("cal-day__day--selected")) {
           dayElement.classList.remove("cal-day__day--selected");
         }
@@ -104,26 +136,32 @@ function calendar(inputMonth) {
         dayElement.classList.remove("cal-day__day--selected");
         dayElement.classList.remove("cal-day__day--today");
       }
-      if (dayCount > moment(lastDay).date()) {
-        dayElement.innerHTML = "";
+      if (dayCount > lastDay.date()) {
+        dayElement.innerText = "";
         dayElement = dayElement.nextElementSibling;
       } else {
-        dayElement.innerHTML = dayCount;
-        dayElement.value = dayElementValue.add(1,'d');
-        if (moment(dateToday).isSame(dayElement.value)) {
+        dayElement.innerText = dayCount;
+        dayElement.value = moment(dayElementValue);
+        dayElementValue.add(1,'d');
+        if (dateToday.isSame(dayElement.value,'day')) {
           dayElement.classList.add("cal-day__day--today");
         }
-        if (selectedDates.some(x => x.getTime() == dayElement.value.getTime())) {
+        if (selectedDates.some(x => x.isSame(dayElement.value,'day'))) {
           dayElement.classList.add("cal-day__day--selected");
         }
         dayElement.addEventListener("click", selectDayByEvent);
-        firstRowElement.innerHTML = "&gt";
+        firstRowElement.innerText = ">";
         dayCount++;
         dayElement = dayElement.nextElementSibling;
       }
     }
   }
 }
+
+/**
+ * 
+ * @param {*} event 
+ */
 function selectDayByEvent(event) {
   if (event.target.classList.contains("cal-day__day--selected")) {
     event.target.classList.remove("cal-day__day--selected");
@@ -134,13 +172,15 @@ function selectDayByEvent(event) {
   }
 }
 
-
-
+/**
+ * 
+ * @param {*} date 
+ */
 function changeSelectedDates(date) {
-  var placeToWrite = document.getElementById("selectedDatesToDisplay");
-  var textToDisplay = "";
-  if (selectedDates.some(x => moment(x).isSame(date))) {
-    var idx = selectedDates.map(Number).indexOf(+date);
+  let placeToWrite = document.getElementById("selectedDatesToDisplay");
+  let textToDisplay = "";
+  if (selectedDates.some(x => moment(x).isSame(date,'day'))) {
+    let idx = selectedDates.map(Number).indexOf(+date);
     if (idx > -1) {
       selectedDates.splice(idx, 1);
     }
@@ -151,9 +191,9 @@ function changeSelectedDates(date) {
   selectedDates = selectedDates.sort(function (date1, date2) {
     return date1.getTime() - date2.getTime();
   });
-  var startRange;
-  var endRange;
-  for (var i = 0; i < selectedDates.length; i++) {
+  let startRange;
+  let endRange;
+  for (let i = 0; i < selectedDates.length; i++) {
     if (i === selectedDates.length - 1) {
       if (Math.abs(selectedDates[i] - selectedDates[i - 1] === 86400000)) {
         textToDisplay += startRange + " - " + selectedDates[i].toLocaleDateString() + ", ";
@@ -183,41 +223,51 @@ function changeSelectedDates(date) {
     }
   }
   textToDisplay = textToDisplay.substring(0, textToDisplay.length - 2);
-  placeToWrite.innerHTML = "";
-  placeToWrite.innerHTML = textToDisplay;
+  placeToWrite.innerText = "";
+  placeToWrite.innerText = textToDisplay;
 }
 
+/**
+ * 
+ */
 function timeCheck() {
-  var startTime = document.getElementById("StartTime").selectedIndex
-  var endTime = document.getElementById("EndTime").selectedIndex
-  var startTimeLabel = document.getElementById("startTimeLabel");
-  var endTimeLabel = document.getElementById("endTimeLabel");
-  var timeWarning = document.getElementById("timeWarning");
+  let startTime = document.getElementById("StartTime").selectedIndex
+  let endTime = document.getElementById("EndTime").selectedIndex
+  let startTimeLabel = document.getElementById("startTimeLabel");
+  let endTimeLabel = document.getElementById("endTimeLabel");
+  let timeWarning = document.getElementById("timeWarning");
   if (endTime <= startTime) {
     startTimeLabel.style.backgroundColor = "#f8d7da";
     endTimeLabel.style.backgroundColor = "#f8d7da";
     timeWarning.classList.add("alert", "alert-danger");
-    timeWarning.innerHTML = "Start Time starts after End Time.";
+    timeWarning.innerText = "Start Time starts after End Time.";
   }
   else {
     startTimeLabel.style.backgroundColor = "";
     endTimeLabel.style.backgroundColor = "";
     timeWarning.classList.remove("alert", "alert-danger");
-    timeWarning.innerHTML = "";
+    timeWarning.innerText = "";
   }
 }
 
+/**
+ * 
+ */
 function returnToToday() {
   displayMonth = new Date(dateToday.setMonth(dateToday.getMonth()));
   calendar(displayMonth);
 }
 
+/**
+ * 
+ * @param {*} dayOfWeekNum 
+ */
 function selectCalendarColumn(dayOfWeekNum) {
-  var rowLoop;
-  var dayLoop;
+  let rowLoop;
+  let dayLoop;
   for (rowLoop = 0; rowLoop < 6; rowLoop++) {
-    var rowDivElement = "row" + rowLoop.toString();
-    var dateElement = document.getElementById(rowDivElement).nextElementSibling;
+    let rowDivElement = "row" + rowLoop.toString();
+    let dateElement = document.getElementById(rowDivElement).nextElementSibling;
     for (dayLoop = 0; dayLoop < dayOfWeekNum; dayLoop++) {
       dateElement = dateElement.nextElementSibling;
     }
@@ -225,46 +275,58 @@ function selectCalendarColumn(dayOfWeekNum) {
   }
 }
 
-
-
+/**
+ * 
+ * @param {*} dayElement 
+ */
 function selectDayByQuickSelect(dayElement) {
   if (dayElement.classList.contains("cal-day__day--selected")) {
     dayElement.classList.remove("cal-day__day--selected");
     changeSelectedDates(dayElement.value);
-  } else if (dayElement.innerHTML != "") {
+  } else if (dayElement.innerText != "") {
     dayElement.classList.add("cal-day__day--selected");
     changeSelectedDates(dayElement.value);
   }
 }
 
+/**
+ * 
+ * @param {*} event 
+ */
 function selectCalendarRow(event) {
-  var dateElement = event.target.nextElementSibling;
-  var dayLoop;
+  let dateElement = event.target.nextElementSibling;
+  let dayLoop;
   for (dayLoop = 0; dayLoop < 7; dayLoop++) {
-    if (dateElement.innerHTML != "") {
+    if (dateElement.innerText != "") {
       selectDayByQuickSelect(dateElement);
     }
     dateElement = dateElement.nextElementSibling;
   }
 }
 
+/**
+ * 
+ */
 function selectEntireMonth() {
-  for (var dayLoop = 0; dayLoop < 7; dayLoop++) {
+  for (let dayLoop = 0; dayLoop < 7; dayLoop++) {
     selectCalendarColumn(dayLoop);
   }
 }
 
+/**
+ * 
+ */
 function submitForm() {
-  var preJsonObject = { "eventName": "", "dateOrDay": "", "startTime": "", "endTime": "", "timeZone": "", "commentToAttendees": "" };
+  let preJsonObject = { "eventName": "", "dateOrDay": "", "startTime": "", "endTime": "", "timeZone": "", "commentToAttendees": "" };
   preJsonObject["eventName"] = document.getElementById("eventName").value;
   if (selectedDates && selectedDates.length) {
     preJsonObject["dateOrDay"] = selectedDates;
   }
   else {
-    var checked = [];
-    var checkboxes = document.getElementsByName("dayOfTheWeek");
+    let checked = [];
+    let checkboxes = document.getElementsByName("dayOfTheWeek");
     checkboxes = Array.prototype.slice.call(checkboxes);
-    for (var i = 0; i < checkboxes.length; i++) {
+    for (let i = 0; i < checkboxes.length; i++) {
       if (checkboxes[i].classList.contains("active")) {
         checked.push(checkboxes[i].firstElementChild.value);
       }
@@ -277,9 +339,9 @@ function submitForm() {
   preJsonObject["commentToAttendees"] = document.getElementById("commentToAttendees").value;
   preJsonObject["id"] = null;
   const jsonObject = JSON.stringify(preJsonObject);
-  var myHeaders = new Headers();
+  let myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
-  var requestOptions = {
+  let requestOptions = {
     method: 'POST',
     headers: myHeaders,
     body: jsonObject,
@@ -292,11 +354,16 @@ function submitForm() {
   .catch(error => console.log('error', error));
 }
 
+/**
+ * 
+ */
 function resetSelectedDates() {
-  var selectedDateElements = document.querySelectorAll(".cal-day__day--selected");
-  for (var i=0; i < selectedDateElements.length; i++) {
+  let selectedDateElements = document.querySelectorAll(".cal-day__day--selected");
+  for (let i=0; i < selectedDateElements.length; i++) {
     selectedDateElements[i].classList.remove("cal-day__day--selected");
   }
   selectedDates = new Array();
-  document.getElementById("selectedDatesToDisplay").innerHTML = "";
+  document.getElementById("selectedDatesToDisplay").innerText = "";
 }
+
+initialSetup();
