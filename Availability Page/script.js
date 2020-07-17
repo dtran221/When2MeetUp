f098382481814791bfb5d74a237d89158d58cd86
Dennis Tran <dtran221@gmail.com>
2020-07-16 23:10:27 -0400

    refactoring availability page to use moment.js

--- Availability Page/script.js
+++ Availability Page/script.js
@@ -3,23 +3,25 @@ eventCreationDetails = {
   "dateOrDay": ["2020-06-23T04:00:00.000Z", "2020-06-24T04:00:00.000Z", "2020-06-25T04:00:00.000Z", "2020-06-26T04:00:00.000Z", "2020-06-27T04:00:00.000Z", "2020-06-28T04:00:00.000Z", "2020-06-29T04:00:00.000Z"],
   "startTime": "9", "endTime": "21", "timeZone": "America/New_York", "commentToAttendees": "commentcomment"
 };
-var ds;
-var signedInUser;
-var signedInUserPassword;
-var availabilitySheet = {};
+let ds;
+let signedInUser;
+let signedInUserPassword;
+let availabilitySheet = {};
 
 function initialSetup() {
 
-  var temp = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  var mySelect = document.getElementById("TimeZone");
+  let timeZoneList = moment.tz.names();
+  let timeZoneSelect = document.getElementById("timeZone");
+  let userTimeZone = moment.tz.guess(Boolean);
 
-  for (var i, j = 0; (i = TimeZone.options[j]); j++) {
-    if (i.value == temp) {
-      TimeZone.selectedIndex = j;
-      break;
-    }
+  for (let zone of timeZoneList) {
+    let selectOption = document.createElement('option');
+    selectOption.textContent = zone;
+    selectOption.value = zone;
+    timeZoneSelect.appendChild(selectOption);
   }
-  var orig = document.getElementById("fillOutAvailabilityHeader");
+  timeZoneSelect.selectedIndex = timeZoneList.indexOf(userTimeZone);
+
   createAvailabilityGrid();
   createViewGrid();
    ds = new DragSelect({
@@ -30,51 +32,43 @@ function initialSetup() {
 }
 
 function createAvailabilityGrid() {
-  var weekday = new Array(7);
-  weekday[0] = "Sunday";
-  weekday[1] = "Monday";
-  weekday[2] = "Tuesday";
-  weekday[3] = "Wednesday";
-  weekday[4] = "Thursday";
-  weekday[5] = "Friday";
-  weekday[6] = "Saturday";
-  var header = eventCreationDetails.dateOrDay;
-  var startTime = parseInt(eventCreationDetails.startTime);
-  var endTime = parseInt(eventCreationDetails.endTime);
+  let dates = eventCreationDetails.dateOrDay;
+  let startTime = parseInt(eventCreationDetails.startTime);
+  let endTime = parseInt(eventCreationDetails.endTime);
   count = 0;
-  var container = document.getElementById("availabilityGrid");
-  var orig = document.getElementById("fillOutColumn0");
-  var cln = orig.cloneNode(true);
-  for (var i = 0; i < header.length; i++) {
-    var date = new Date(header[i]);
-    var previousLocation = document.getElementById("fillOutColumn" + count.toString());
+  let container = document.getElementById("availabilityGrid");
+  let orig = document.getElementById("fillOutColumn0");
+  let cln = orig.cloneNode(true);
+  for (let i = 0; i < dates.length; i++) {
+    let date = moment(Date(dates[i]));
+    let previousLocation = document.getElementById("fillOutColumn" + count.toString());
     count++;
     cln.id = "fillOutColumn" + count.toString();
     cln.classList.add("d-inline-flex")
     cln.classList.add("justify-content-center")
     cln.classList.add("noSelect");
     cln.style.textAlign = "center";
-    cln.innerHTML = date.toLocaleDateString() + "<br/>" + weekday[date.getDay()].substring(0,3);
+    cln.innertext = date.format("M/D/YYYY") + "<br/>" + date.format("ddd");
     cln.value = date;
     previousLocation.insertAdjacentElement("afterend", cln);
     cln = orig.cloneNode(true);
   }
-  var newDiv = document.createElement("div");
+  let newDiv = document.createElement("div");
   newDiv.classList.add("w-100");
   container.appendChild(newDiv);
-  var timeToShow = startTime;
-  var timeToShowId = startTime * 100;
-  for (var i = 0; i < endTime - startTime; i++) {
-    var timeDiv = document.createElement("div");
+  let timeToShow = startTime;
+  let timeToShowId = startTime * 100;
+  for (let i = 0; i < endTime - startTime; i++) {
+    let timeDiv = document.createElement("div");
     timeDiv.id = "fillOutRowTime" + timeToShowId.toString();
     if (timeToShow > 12) {
-      timeDiv.innerHTML = (timeToShow - 12).toString() + " PM"
+      timeDiv.innertext = (timeToShow - 12).toString() + " PM"
     }
     else if (timeToShow == 12) {
-      timeDiv.innerHTML = timeDiv.innerHTML = timeToShow.toString() + " PM"
+      timeDiv.innertext = timeToShow.toString() + " PM"
     }
     else {
-      timeDiv.innerHTML = (timeToShow).toString() + " AM";
+      timeDiv.innertext = (timeToShow).toString() + " AM";
     }
     timeDiv.classList.add("d-inline-flex");
     timeDiv.classList.add("justify-content-center");
@@ -82,14 +76,14 @@ function createAvailabilityGrid() {
     timeDiv.classList.add("noSelect");
     timeDiv.classList.add("no-gutters");
     container.appendChild(timeDiv);
-    for (var j = 1; j < header.length +1; j++) {
-      var date = new Date(header[j]);
-      var fillDiv = document.createElement("div");
+    for (let j = 1; j < dates.length +1; j++) {
+      let date = moment(Date(dates[j]));
+      let fillDiv = document.createElement("div");
       fillDiv.classList.add("col");
       fillDiv.classList.add("border");
       fillDiv.classList.add("border-dark");
       fillDiv.classList.add("no-gutters");
-      if (j != (header.length)) {
+      if (j != (dates.length)) {
         fillDiv.classList.add("border-right-0");
       }
       if (i != (endTime - startTime - 1)) {
@@ -98,7 +92,7 @@ function createAvailabilityGrid() {
       fillDiv.id = "fillOutRow" + (timeToShowId.toString().length == 3 ? "0":"") + timeToShowId.toString() + "Column" + j.toString();
       container.appendChild(fillDiv);
     }
-    var controllWidthDiv = document.createElement("div");
+    let controllWidthDiv = document.createElement("div");
     controllWidthDiv.classList.add("w-100");
 
     container.appendChild(controllWidthDiv);
@@ -107,20 +101,24 @@ function createAvailabilityGrid() {
   }
   
   timeToShowId = startTime * 100;
-  for (var i = 1; i < (header.length +1); i++) {
-    var rowCount = 0;
-    for (j = 0; j < endTime - startTime; j++) {
-      var selectedTimeDateDiv = document.getElementById("fillOutRow" + (timeToShowId.toString().length == 3 ? "0":"") + timeToShowId.toString() + "Column" + i.toString());
-      for (var k = 0; k < 4; k++) {
-        var fillDiv = document.createElement("div");
+  for (let i = 1; i < (dates.length +1); i++) {
+    let divId = moment(dates[i]);
+    let rowCount = 0;
+    divId.hour(startTime);
+    for (let j = 0; j < endTime - startTime; j++) {
+      
+      let selectedTimeDateDiv = document.getElementById("fillOutRow" + (timeToShowId.toString().length == 3 ? "0":"") + timeToShowId.toString() + "Column" + i.toString());
+      for (let k = 0; k < 4; k++) {
+        let fillDiv = document.createElement("div");
         fillDiv.classList.add("h-25");
         if (k == 1) {
           fillDiv.classList.add("halfWayLine");
         }
         fillDiv.id = "fillOutRow" + rowCount.toString() + "Column" + i.toString();
-        var dateTimeForValue = document.getElementById("fillOutColumn" + i.toString()).value;
-        var hour = parseInt(((timeToShowId.toString().length == 3 ? "0":"") + timeToShowId.toString()).substring(0,2));
-        var minutes = parseInt(((timeToShowId.toString().length == 3 ? "0":"") + timeToShowId.toString()).substring(2));
+        let something = document.getElementById("fillOutColumn" + i.toString())
+        let dateTimeForValue = something.value;
+        let hour = parseInt(((timeToShowId.toString().length == 3 ? "0":"") + timeToShowId.toString()).substring(0,2));
+        let minutes = parseInt(((timeToShowId.toString().length == 3 ? "0":"") + timeToShowId.toString()).substring(2));
         dateTimeForValue.setHours(hour,minutes);
         fillDiv.value = dateTimeForValue;
         fillDiv.addEventListener("mousedown",(event) => event.preventDefault());
@@ -147,7 +145,7 @@ function displayView() {
 }
 
 function selectAll() {
-  var selectableNodes = document.getElementsByClassName("selectable-nodes");
+  let selectableNodes = document.getElementsByClassName("selectable-nodes");
   ds.setSelectables(selectableNodes,false,true);
 }
 
@@ -165,13 +163,13 @@ function signInUser() {
 }
 
 function submitAvailabilityForm(){
-  var something = ds.getSelection();
+  let something = ds.getSelection();
   availabilitySheet[signedInUser] = ds.getSelection();
   //TODO: Post data to server
 }
 
 function createViewGrid() {
-  var weekday = new Array(7);
+  let weekday = new Array(7);
   weekday[0] = "Sunday";
   weekday[1] = "Monday";
   weekday[2] = "Tuesday";
@@ -179,16 +177,16 @@ function createViewGrid() {
   weekday[4] = "Thursday";
   weekday[5] = "Friday";
   weekday[6] = "Saturday";
-  var header = eventCreationDetails.dateOrDay;
-  var startTime = parseInt(eventCreationDetails.startTime);
-  var endTime = parseInt(eventCreationDetails.endTime);
+  let header = eventCreationDetails.dateOrDay;
+  let startTime = parseInt(eventCreationDetails.startTime);
+  let endTime = parseInt(eventCreationDetails.endTime);
   count = 0;
-  var container = document.getElementById("viewGrid");
-  var orig = document.getElementById("viewColumn0");
-  var cln = orig.cloneNode(true);
-  for (var i = 0; i < header.length; i++) {
-    var date = new Date(header[i]);
-    var previousLocation = document.getElementById("viewColumn" + count.toString());
+  let container = document.getElementById("viewGrid");
+  let orig = document.getElementById("viewColumn0");
+  let cln = orig.cloneNode(true);
+  for (let i = 0; i < header.length; i++) {
+    let date = new Date(header[i]);
+    let previousLocation = document.getElementById("viewColumn" + count.toString());
     count++;
     cln.id = "viewColumn" + count.toString();
     cln.classList.add("d-inline-flex")
@@ -200,22 +198,22 @@ function createViewGrid() {
     previousLocation.insertAdjacentElement("afterend", cln);
     cln = orig.cloneNode(true);
   }
-  var newDiv = document.createElement("div");
+  let newDiv = document.createElement("div");
   newDiv.classList.add("w-100");
   container.appendChild(newDiv);
-  var timeToShow = startTime;
-  var timeToShowId = startTime * 100;
-  for (var i = 0; i < endTime - startTime; i++) {
-    var timeDiv = document.createElement("div");
+  let timeToShow = startTime;
+  let timeToShowId = startTime * 100;
+  for (let i = 0; i < endTime - startTime; i++) {
+    let timeDiv = document.createElement("div");
     timeDiv.id = "viewRowTime" + timeToShowId.toString();
     if (timeToShow > 12) {
-      timeDiv.innerHTML = (timeToShow - 12).toString() + " PM"
+      timeDiv.innertext = (timeToShow - 12).toString() + " PM"
     }
     else if (timeToShow == 12) {
-      timeDiv.innerHTML = timeDiv.innerHTML = timeToShow.toString() + " PM"
+      timeDiv.innertext = timeToShow.toString() + " PM"
     }
     else {
-      timeDiv.innerHTML = (timeToShow).toString() + " AM";
+      timeDiv.innertext = (timeToShow).toString() + " AM";
     }
     timeDiv.classList.add("d-inline-flex");
     timeDiv.classList.add("justify-content-center");
@@ -223,9 +221,9 @@ function createViewGrid() {
     timeDiv.classList.add("noSelect");
     timeDiv.classList.add("no-gutters");
     container.appendChild(timeDiv);
-    for (var j = 1; j < header.length +1; j++) {
-      var date = new Date(header[j]);
-      var fillDiv = document.createElement("div");
+    for (let j = 1; j < header.length +1; j++) {
+      let date = new Date(header[j]);
+      let fillDiv = document.createElement("div");
       fillDiv.classList.add("col");
       fillDiv.classList.add("border");
       fillDiv.classList.add("border-dark");
@@ -239,7 +237,7 @@ function createViewGrid() {
       fillDiv.id = "viewRow" + (timeToShowId.toString().length == 3 ? "0":"") + timeToShowId.toString() + "Column" + j.toString();
       container.appendChild(fillDiv);
     }
-    var controllWidthDiv = document.createElement("div");
+    let controllWidthDiv = document.createElement("div");
     controllWidthDiv.classList.add("w-100");
 
     container.appendChild(controllWidthDiv);
@@ -248,20 +246,20 @@ function createViewGrid() {
   }
   
   timeToShowId = startTime * 100;
-  for (var i = 1; i < (header.length +1); i++) {
-    var rowCount = 0;
+  for (let i = 1; i < (header.length +1); i++) {
+    let rowCount = 0;
     for (j = 0; j < endTime - startTime; j++) {
-      var selectedTimeDateDiv = document.getElementById("viewRow" + (timeToShowId.toString().length == 3 ? "0":"") + timeToShowId.toString() + "Column" + i.toString());
-      for (var k = 0; k < 4; k++) {
-        var fillDiv = document.createElement("div");
+      let selectedTimeDateDiv = document.getElementById("viewRow" + (timeToShowId.toString().length == 3 ? "0":"") + timeToShowId.toString() + "Column" + i.toString());
+      for (let k = 0; k < 4; k++) {
+        let fillDiv = document.createElement("div");
         fillDiv.classList.add("h-25");
         if (k == 1) {
           fillDiv.classList.add("halfWayLine");
         }
         fillDiv.id = "viewRow" + rowCount.toString() + "Column" + i.toString();
-        var dateTimeForValue = document.getElementById("viewColumn" + i.toString()).value;
-        var hour = parseInt(((timeToShowId.toString().length == 3 ? "0":"") + timeToShowId.toString()).substring(0,2));
-        var minutes = parseInt(((timeToShowId.toString().length == 3 ? "0":"") + timeToShowId.toString()).substring(2));
+        let dateTimeForValue = document.getElementById("viewColumn" + i.toString()).value;
+        let hour = parseInt(((timeToShowId.toString().length == 3 ? "0":"") + timeToShowId.toString()).substring(0,2));
+        let minutes = parseInt(((timeToShowId.toString().length == 3 ? "0":"") + timeToShowId.toString()).substring(2));
         dateTimeForValue.setHours(hour,minutes);
         fillDiv.value = dateTimeForValue;
         fillDiv.addEventListener("mousedown",(event) => event.preventDefault());
@@ -276,11 +274,15 @@ function createViewGrid() {
 }
 
 function fillOutViewGrid() {
-  for (var key in availabilitySheet) {
-    var selectedDateTime = availabilitySheet[key];
-    for (var dateTime in selectedDateTime) {
+  for (let key in availabilitySheet) {
+    let selectedDateTime = availabilitySheet[key];
+    for (let dateTime in selectedDateTime) {
       document.querySelector([value = dateTime]).style.backgroundColor = "green";
     }
     
   }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  initialSetup();
+});
