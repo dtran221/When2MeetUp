b4a389cefd7562b00a7c28568b1a4ccc381e2347
Dennis Tran <dtran221@gmail.com>
2020-08-20 21:45:52 -0400

    added external libraries and input fields

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -22,6 +22,24 @@
         </v-btn-toggle>
     <div v-if="dateDayToggle === 'Dates in a Month'"> placeholder for dates in month</div>
     <div v-else> placeholder for days of the week</div>
+    <v-select
+          :items="timeSelect"
+          label="Start Time: "
+          v-model="startTime"
+          outlined
+        ></v-select>
+    <v-select
+          :items="timeSelect"
+          label="End Time: "
+          v-model="endTime"
+          outlined
+        ></v-select>
+    <v-textarea
+          outlined
+          name="input-7-4"
+          label="Comment for Attendees"
+          v-model="commentFromCreator"
+        ></v-textarea>
   </div>
 </template>
 
@@ -33,11 +51,12 @@ export default {
   data () {
     return {
       eventName: '',
-      startTime:0,
-      endTime:0,
+      startTime:'9 AM',
+      endTime:'9 PM',
       timeZone:'',
       commentFromCreator: '',
       dateDayToggle: 'Dates in a Month',
+      timeSelect: ['12 AM', '1 AM', '2 AM', '3 AM', '4 AM', '5 AM', '6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM', '9 PM', '10 PM', '11 PM', '12 AM']
     }
   }
 }
