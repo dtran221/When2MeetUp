e2583406758a9391830181476281524bbc07e176
Dennis Tran <dtran221@gmail.com>
2020-08-30 14:36:13 -0400

    styling changes

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,69 +1,106 @@
 <template>
   <v-container class="home">
-    <v-row justify="center">
-      <v-text-field
-        v-model="eventName"
-        label="Event Name"
-        placeholder="Study Night"
-        outlined
-        clearable
-        required
-      ></v-text-field>
-    </v-row>
-    <v-row justify="center">
-      <h1>Potential dates/days for the event</h1>
-    </v-row>
-    <v-row justify="center">
-      <v-btn-toggle v-model="dateDayToggle" mandatory>
-        <v-btn value="Dates in a Month" v-on:click="clearDatesOrDays()">Dates in a Month</v-btn>
-        <v-btn value="Days of the Week" v-on:click="clearDatesOrDays()">Days of the Week</v-btn>
-      </v-btn-toggle>
-    </v-row>
-    <v-row justify="center">
-      <v-container v-if="dateDayToggle === 'Dates in a Month'">
-        <v-date-picker
-        v-model="selectedDates"
-        multiple
-      ></v-date-picker>
-      </v-container>
-      <v-container v-else>
-        <v-row justify="center" class= "mb-3">
-          <v-btn-toggle v-model="selectedDays" multiple>
-            <v-btn value="Sun Jan 13 1980 00:00:00 GMT-0500 (Eastern Standard Time)">Sun</v-btn>
-            <v-btn value="Mon Jan 14 1980 00:00:00 GMT-0500 (Eastern Standard Time)">Mon</v-btn>
-            <v-btn value="Tue Jan 15 1980 00:00:00 GMT-0500 (Eastern Standard Time)">Tue</v-btn>
-            <v-btn value="Wed Jan 16 1980 00:00:00 GMT-0500 (Eastern Standard Time)">Wed</v-btn>
-            <v-btn value="Thu Jan 17 1980 00:00:00 GMT-0500 (Eastern Standard Time)">Thu</v-btn>
-            <v-btn value="Fri Jan 18 1980 00:00:00 GMT-0500 (Eastern Standard Time)">Fri</v-btn>
-            <v-btn value="Sat Jan 19 1980 00:00:00 GMT-0500 (Eastern Standard Time)">Sat</v-btn>
-          </v-btn-toggle>
-        </v-row>
-        <v-row justify="center">
-          <v-btn-toggle v-model="toggle_exclusive">
-            <v-btn v-on:click="selectAllDaysOfWeek()">All</v-btn>
-            <v-btn v-on:click="selectWeekdays()">Weekdays</v-btn>
-            <v-btn v-on:click="selectWeekend()">Weekend</v-btn>
-          </v-btn-toggle>
-        </v-row>
-      </v-container>
-    </v-row>
-    <v-row justify="center">
-      <v-select :items="timeSelect" label="Start Time: " v-model="startTime" outlined></v-select>
-    </v-row>
-    <v-row justify="center">
-      <v-select :items="timeSelect" label="End Time: " v-model="endTime" outlined></v-select>
-    </v-row>
-    <v-row justify="center">
-      <v-select :items="timeZones" label="Time Zone: " v-model="selectedTimeZone" outlined></v-select>
-    </v-row>
-    <v-row justify="center">
-      <v-textarea
-        outlined
-        name="input-7-4"
-        label="Comment for Attendees"
-        v-model="commentFromCreator"
-      ></v-textarea>
-    </v-row>
+    <v-form
+    ref="form"
+      v-model="valid"
+      :lazy-validation="lazy">
+      <v-row justify="center">
+        <v-col cols="9">
+          <v-text-field v-model="eventName" label="Event Name" outlined clearable required :rules="[v => !!v || 'Event Name is required']"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <h1>Potential dates/days for the event</h1>
+      </v-row>
+      <v-row justify="center" class="mb-3">
+        <v-btn-toggle v-model="dateDayToggle" mandatory>
+          <v-btn value="Dates in a Month" v-on:click="clearDatesOrDays()">Dates in a Month</v-btn>
+          <v-btn value="Days of the Week" v-on:click="clearDatesOrDays()">Days of the Week</v-btn>
+        </v-btn-toggle>
+      </v-row>
+      <v-row justify="center">
+        <v-container v-if="dateDayToggle === 'Dates in a Month'">
+          <v-row justify="center" class="mb-3">
+            <v-date-picker
+              :landscape="$vuetify.breakpoint.smAndUp"
+              :min="minimumDate"
+              :scrollable="true"
+              v-model="selectedDates"
+              multiple
+              required
+              :rules="[v => !!v || 'Date is required']"
+            ></v-date-picker>
+          </v-row>
+          <v-row justify="center">
+            <v-btn-toggle v-model="toggle_exclusive">
+              <v-btn v-on:click="resetSelectedDates()">Reset</v-btn>
+            </v-btn-toggle>
+          </v-row>
+        </v-container>
+        <v-container v-else>
+          <v-row justify="center" class="mb-3">
+            <v-btn-toggle v-model="selectedDays" multiple>
+              <v-btn value="Sun Jan 13 1980 00:00:00 GMT-0500 (Eastern Standard Time)">Sun</v-btn>
+              <v-btn value="Mon Jan 14 1980 00:00:00 GMT-0500 (Eastern Standard Time)">Mon</v-btn>
+              <v-btn value="Tue Jan 15 1980 00:00:00 GMT-0500 (Eastern Standard Time)">Tue</v-btn>
+              <v-btn value="Wed Jan 16 1980 00:00:00 GMT-0500 (Eastern Standard Time)">Wed</v-btn>
+              <v-btn value="Thu Jan 17 1980 00:00:00 GMT-0500 (Eastern Standard Time)">Thu</v-btn>
+              <v-btn value="Fri Jan 18 1980 00:00:00 GMT-0500 (Eastern Standard Time)">Fri</v-btn>
+              <v-btn value="Sat Jan 19 1980 00:00:00 GMT-0500 (Eastern Standard Time)">Sat</v-btn>
+            </v-btn-toggle>
+          </v-row>
+          <v-row justify="center">
+            <v-btn-toggle v-model="toggle_exclusive">
+              <v-btn v-on:click="selectAllDaysOfWeek()">All</v-btn>
+              <v-btn v-on:click="selectWeekdays()">Weekdays</v-btn>
+              <v-btn v-on:click="selectWeekend()">Weekend</v-btn>
+            </v-btn-toggle>
+          </v-row>
+        </v-container>
+      </v-row>
+
+      <v-row justify="center">
+        <v-col cols="9">
+          <v-select :items="timeSelect" label="Start Time: " v-model="startTime" outlined required :rules="[v => !!v || 'Start Time is required']"></v-select>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="9">
+          <v-select :items="timeSelect" label="End Time: " v-model="endTime" outlined required :rules="[v => !!v || 'End Time is required']"></v-select>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="9">
+          <v-select :items="timeZones" label="Time Zone: " v-model="selectedTimeZone" outlined required :rules="[v => !!v || 'Time Zone is required']"></v-select>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="9">
+          <v-textarea
+            outlined
+            name="input-7-4"
+            label="Comment for Attendees"
+            v-model="commentFromCreator"
+          ></v-textarea>
+        </v-col>
+      </v-row>
+      <v-btn
+      :disabled="!valid"
+      color="success"
+      class="mr-4"
+      @click="validate"
+    >
+      Submit
+    </v-btn>
+
+    <v-btn
+      color="error"
+      class="mr-4"
+      @click="reset"
+    >
+      Reset Form
+    </v-btn>
+    </v-form>
   </v-container>
 </template>
 
@@ -74,6 +111,7 @@ export default {
   name: "Home",
   data() {
     return {
+      valid: true,
       eventName: "",
       startTime: "9 AM",
       endTime: "9 PM",
@@ -111,6 +149,7 @@ export default {
       selectedTimeZone: "",
       selectedDates: [],
       selectedDays: [],
+      minimumDate: moment().toISOString(),
     };
   },
   created() {
@@ -189,12 +228,23 @@ export default {
       }
     },
     clearDatesOrDays() {
-      if(this.dateDayToggle === "Dates in a Month") {
+      if (this.dateDayToggle === "Dates in a Month") {
         this.selectedDays = [];
       } else {
         this.selectedDates = [];
       }
     },
+    resetSelectedDates() {
+      this.selectedDates = [];
+    },
+    validate () {
+        this.$refs.form.validate()
+      },
+      reset () {
+        this.$refs.form.reset();
+        this.selectedDates = [];
+        this.selectedDays = [];
+      },
   },
 };
 </script>
